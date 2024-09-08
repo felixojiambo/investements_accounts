6b1d90e7f1e434eb2cd2a554d0abc6fa95dc749c
felixojiambo <felixojiamboe@gmail.com>
2024-09-08 12:24:51 +0300

    Implement AdminTransactionView for admins to view all user transactions with an optional date filter and total balance aggregation.

--- accounts/views.py
+++ accounts/views.py
@@ -21,3 +21,25 @@ class TransactionCreateView(generics.CreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = [IsAuthenticated, IsAccountPostOnly]
+
+class AdminTransactionView(generics.ListAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get_queryset(self):
+        user = self.request.user
+        transactions = Transaction.objects.filter(account__userinvestmentaccount__user=user)
+
+        # Apply date filter if provided
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+        if start_date and end_date:
+            transactions = transactions.filter(created_at__range=[start_date, end_date])
+
+        return transactions
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        total_balance = self.get_queryset().aggregate(Sum('account__balance'))['account__balance__sum']
+        response.data = {'transactions': response.data, 'total_balance': total_balance}
+        return response
