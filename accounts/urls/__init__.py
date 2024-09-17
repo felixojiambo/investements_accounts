eb1fcbf29e32a3fd99195fa59e56bdb35e5dca17
felixojiambo <felixojiamboe@gmail.com>
2024-09-17 12:07:21 +0300

    package init for urls folder

---
+++ accounts/urls/__init__.py
@@ -0,0 +1,9 @@
+
+from django.urls import include, path
+
+urlpatterns = [
+    path('auth/', include('accounts.urls.authentication_urls')),
+    path('user-accounts/', include('accounts.urls.user_account_urls')),
+    path('investment-accounts/', include('accounts.urls.investment_account_urls')),
+    path('transactions/', include('accounts.urls.transaction_urls')),
+]
